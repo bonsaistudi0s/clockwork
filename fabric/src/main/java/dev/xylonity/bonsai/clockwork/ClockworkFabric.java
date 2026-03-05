1d788515ccecc0d095c5747013c56252ed96aa67
xylonity <42015327+Xylonity@users.noreply.github.com>
2026-03-05 14:48:28 +0100

    🀄 Refactor fabric initializers into two separated classes

--- fabric/src/main/java/dev/xylonity/bonsai/clockwork/ClockworkFabric.java
+++ fabric/src/main/java/dev/xylonity/bonsai/clockwork/ClockworkFabric.java
@@ -1,18 +1,29 @@
 package dev.xylonity.bonsai.clockwork;
 
-import net.fabricmc.api.ClientModInitializer;
+import dev.xylonity.bonsai.clockwork.common.CommonProxy;
+import dev.xylonity.bonsai.clockwork.common.event.ClockworkServerEvents;
+import dev.xylonity.bonsai.clockwork.config.ClockworkConfig;
+import dev.xylonity.bonsai.clockwork.registry.ClockworkPackets;
+import dev.xylonity.knightlib.api.config.ConfigComposer;
+import dev.xylonity.knightlib.api.event.KnightLibEvents;
 import net.fabricmc.api.ModInitializer;
 
-public class ClockworkFabric implements ModInitializer, ClientModInitializer {
+public class ClockworkFabric implements ModInitializer {
     
     @Override
     public void onInitialize() {
+
+        Clockwork.PROXY = new CommonProxy();
+
+        // Config registrar
+        ConfigComposer.registerConfig(Clockwork.MOD_ID, ClockworkConfig.class);
+
+        // Event registrar
+        KnightLibEvents.SERVER.register(ClockworkServerEvents.class);
+
+        ClockworkPackets.registerC2S();
+
         Clockwork.init();
-    }
-
-    @Override
-    public void onInitializeClient() {
-
     }
 
 }
---
+++ fabric/src/main/java/dev/xylonity/bonsai/clockwork/ClockworkFabricClient.java
@@ -0,0 +1,23 @@
+package dev.xylonity.bonsai.clockwork;
+
+import dev.xylonity.bonsai.clockwork.client.ClientProxy;
+import dev.xylonity.bonsai.clockwork.client.event.ClockworkClientEvents;
+import dev.xylonity.bonsai.clockwork.registry.ClockworkPackets;
+import dev.xylonity.knightlib.api.event.KnightLibEvents;
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClockworkFabricClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+        Clockwork.PROXY = new ClientProxy();
+
+        // Event registrar
+        KnightLibEvents.CLIENT.register(ClockworkClientEvents.class);
+
+        ClockworkPackets.registerS2C();
+
+    }
+
+}
