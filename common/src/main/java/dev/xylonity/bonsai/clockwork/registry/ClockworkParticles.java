aad512efbaefd1460b8c3e72c289b42461164b47
xylonity <42015327+Xylonity@users.noreply.github.com>
2026-03-18 02:50:02 +0100

    🀄 Added some missing assets

--- common/src/main/java/dev/xylonity/bonsai/clockwork/registry/ClockworkParticles.java
+++ common/src/main/java/dev/xylonity/bonsai/clockwork/registry/ClockworkParticles.java
@@ -2,13 +2,12 @@ package dev.xylonity.bonsai.clockwork.registry;
 
 import com.mojang.serialization.Codec;
 import dev.xylonity.bonsai.clockwork.Clockwork;
+import dev.xylonity.bonsai.clockwork.client.particle.FlamethrowerParticleData;
 import dev.xylonity.bonsai.clockwork.client.particle.PotionSprayParticleData;
-import dev.xylonity.knightlib.KnightLib;
 import dev.xylonity.knightlib.api.registrar.ResourceDispatcher;
 import dev.xylonity.knightlib.api.registrar.ResourceEntry;
 import dev.xylonity.knightlib.api.registrar.ResourceRegistry;
 import net.minecraft.core.particles.ParticleType;
-import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.core.registries.BuiltInRegistries;
 
 public class ClockworkParticles {
@@ -20,6 +19,15 @@ public class ClockworkParticles {
                 @Override
                 public Codec<PotionSprayParticleData> codec() {
                     return PotionSprayParticleData.CODEC;
+                }
+
+            });
+
+    public static final ResourceEntry<ParticleType<FlamethrowerParticleData>> FLAMETHROWER_PARTICLE =
+            PARTICLES.register("flamethrower", () -> new ParticleType<>(false, FlamethrowerParticleData.DESERIALIZER) {
+                @Override
+                public Codec<FlamethrowerParticleData> codec() {
+                    return FlamethrowerParticleData.CODEC;
                 }
 
             });
